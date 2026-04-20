58854eb8854408fa8b8f67f12c039d3027ce92f4
Arif <arifraffyfadlurahman@gmail.com>
2026-04-20 12:57:08 +0700

    feat: menambahkan warna dan benerin data json juga panggilannya di page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,29 @@ import React, {useState, useEffect} from "react"
 import axios from "axios"
 
 export interface Data {
-    id:     number;
-    name:   string;
-    skills: string[];
+    ID:          number;
+    uuid:        string;
+    name:        string;
+    description: string;
+    skills:      string[];
+    projects:    string[];
+    careers:     string[];
+    colors:      Colors;
 }
+
+export interface Colors {
+    base_color:      string;
+    primary_color:   string;
+    secondary_color: string;
+    subtle_color:    string;
+}
+
+
+
 export default function Home() {
     const [jurusan, setJurusan] = useState <Data[]>([])
     const [pilihan, setPilihan] = useState <Data>();
+    const [projects, setProjects] = useState <Data>();
 
     useEffect (() => {
         axios.get<Data[]>('/majors.json')
@@ -26,7 +42,7 @@ return (
 
         <ul>
             {jurusan.map((item : any) => (
-                <li key={item.id} onClick={() => setPilihan (item)} style={{cursor: 'pointer', padding: '10px', border: '1px solid #ccc', margin: '5px 0'}}>
+                <li key={item.ID} onClick={() => setPilihan (item)} style={{cursor: 'pointer', padding: '10px', border: '1px solid #ccc', margin: '5px 0'}}>
                     {item.name} (Klik buat liat skill)
                 </li>
             ))}
@@ -34,11 +50,30 @@ return (
         <hr />
 
         {pilihan && (
-            <div>
+            <div className={``} style={{backgroundColor:pilihan.colors.secondary_color}}>
+                <h2>Deskripsi {pilihan.name}:</h2>
+                <ul>
+                        <li>{pilihan.description}</li>
+                </ul>
+                <br />
                 <h2>Skill di jurusan {pilihan.name}:</h2>
                 <ul>
                     {pilihan.skills?.map((skill: string, index: number) => (
                         <li key={index}>{skill}</li>
+                    ))}
+                </ul>
+                <br />
+                <h2>Projek di {pilihan.name}:</h2>
+                <ul>
+                    {pilihan.projects.map((proyek: string, index: number) => (
+                        <li key={index}>{proyek}</li>
+                    ))}
+                </ul>
+                <br />
+                 <h2>Career di {pilihan.name}:</h2>
+                <ul>
+                    {pilihan.careers.map((karir: string, index: number) => (
+                        <li key={index}>{karir}</li>
                     ))}
                 </ul>
             </div>
